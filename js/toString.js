57a71d8d40ee233c3b5101ededd90e60be106ba6
JuliaObeid <78361535+JuliaObeid@users.noreply.github.com>
2021-06-21 19:05:16 +0300

    toString

---
+++ js/toString.js
@@ -0,0 +1,53 @@
+// === TO STRING ===
+
+// ЧИСЛО К СТРОКЕ
+let value = 0;
+
+// 1й способ
+let toString = String(value)
+console.log('${value},через конструктор String(): ', toString)
+console.log('тип данных ${value},через конструктор String(): ',typeof toString)
+
+// 2й способ
+toString = value + ""
+console.log('${value},через конкатенацию: ', toString)
+console.log('тип данных ${value},через конкатенацию: ', typeof toString)
+
+value = 1;
+
+// 1й способ
+toString = String(value)
+console.log('${value},через конструктор String(): ', toString)
+console.log('тип данных ${value},через конструктор String(): ',typeof toString)
+
+// 2й способ
+toString = value + ""
+console.log('${value},через конкатенацию: ', toString)
+console.log('тип данных ${value},через конкатенацию: ', typeof toString)
+
+value = Infinity;
+
+// 1й способ
+toString = String(value)
+console.log('${value},через конструктор String(): ', toString)
+console.log('тип данных ${value},через конструктор String(): ',typeof toString)
+
+// 2й способ
+toString = value + ""
+console.log('${value},через конкатенацию: ', toString)
+console.log('тип данных ${value},через конкатенацию: ', typeof toString)
+
+value = NaN
+
+// 1й способ
+toString = String(value)
+console.log('${value},через конструктор String(): ', toString)
+console.log('тип данных ${value},через конструктор String(): ',typeof toString)
+
+// 2й способ
+toString = value + ""
+console.log('${value},через конкатенацию: ', toString)
+console.log('тип данных ${value},через конкатенацию: ', typeof toString)
+
+// БУЛЕВОЕ (ЛОГИЧЕСКОЕ) true к строке
+value = true
